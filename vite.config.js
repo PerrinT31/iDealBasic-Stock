8da1943d5748f3adfbae7fbde4bbdfa98432d170
PerrinT31 <thomas.perrin@toptex.fr>
2025-09-25 09:18:52 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,1 +1,15 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    port: 5173, // Port par défaut (modifiable si besoin)
+    open: true  // Ouvre automatiquement l'app dans le navigateur
+  },
+  build: {
+    outDir: "dist",
+    sourcemap: false
+  }
+});
 
